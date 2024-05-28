396f2fb6ae0ece05f266826f7cf663823c186c45
Ahmed Khedr <94747792+Developer-Ahmed-Ayman-khedr@users.noreply.github.com>
2024-05-28 22:34:44 +0300

    Initialize and upload SS driver.

---
+++ SourceCode/HAL/SS/SS_INT.h
@@ -0,0 +1,49 @@
+/*
+ * SS_INT.h
+ *
+ *  Created on: May 28, 2024
+ *      Author: ahmed
+ */
+
+#ifndef HAL_SS_SS_INT_H_
+#define HAL_SS_SS_INT_H_
+
+
+/******************************************
+  INCLUDES
+*******************************************/
+
+#include "DIO_INT.h"
+
+/******************************************
+  Global Data TYPES AND STRUCTURES
+*******************************************/
+
+
+/******************************************
+  GLOBAL CONSTANT MACROS
+*******************************************/
+
+#define SS_S0 DIO_PINA4
+#define SS_S1 DIO_PINA5
+#define SS_S2 DIO_PINA6
+#define SS_S3 DIO_PINA7
+
+#define SS_ENABLE1 DIO_PINB2
+#define SS_ENABLE2 DIO_PINB1
+
+/******************************************
+  GLOBAL FUNCTIONS MACROS
+*******************************************/
+
+
+/******************************************
+  GLOBAL FUNCTIONS Prototypes
+*******************************************/
+//Initialize
+void SS_init();
+
+//Send
+void SS_writeNum(u8 num);
+
+#endif /* HAL_SS_SS_INT_H_ */
